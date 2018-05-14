1ce8275ee31b62e74800ae8a203e36fcafd7794a
Brendan Arciszewski <arciszewski.bd@gmail.com>
2018-05-14 18:22:04 -0400

    Auto Generate ALT from TITLE of IMG

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+	$("img").each( function () {
+		$(this).attr("alt", $(this).attr("title"));
+	});
+	
 	$("a.show-hidden").on("click", function () {
 		if ($(this).next().hasClass("hide")) {
 			$(this).next().removeClass("hide");
